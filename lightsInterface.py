ec0d84b47fc267535518ac0ab0e130133ec7e026
ConnerPappasWebDev <63215665+PlatyPappas@users.noreply.github.com>
2021-07-30 22:38:45 -0700

    Update lightsInterface.py

--- lightsInterface.py
+++ lightsInterface.py
@@ -11,12 +11,10 @@ class SupportThreadController:
   def __init__(self):
     self.colorThreadToggle = True
     self.modeThreadToggle = True
-    self.pulseThreadToggle = True
     self.brightnessThreadToggle = True
 
     self.colorThread = threading.Thread(target=self.colorThreadOperation)
     self.modeThread = threading.Thread(target=self.modeThreadOperation)
-    self.pulseThread = threading.Thread(target=self.pulseThreadOperation)
     self.brightnessThread = threading.Thread(target=self.brightnessThreadOperation)
 
     self.colorContext = zmq.Context()
@@ -24,9 +22,6 @@ class SupportThreadController:
 
     self.modeContext = zmq.Context()
     self.modeSocket = self.modeContext.socket(zmq.REP)
-
-    self.pulseContext = zmq.Context()
-    self.pulseSocket = self.pulseContext.socket(zmq.REP)
 
     self.brightnessContext = zmq.Context()
     self.brightnessSocket = self.brightnessContext.socket(zmq.REP)
@@ -57,19 +52,13 @@ class SupportThreadController:
     modeSocket.connect("tcp://127.0.0.1:%s" % "2556")
     modeSocket.send_string("WIPE")
 
-    pulseContext = zmq.Context()
-    pulseSocket = pulseContext.socket(zmq.REQ)
-    pulseSocket.connect("tcp://127.0.0.1:%s" % "2557")
-    pulseSocket.send_string("FOO")
-
     brightnessContext = zmq.Context()
     brightnessSocket = brightnessContext.socket(zmq.REQ)
-    brightnessSocket.connect("tcp://127.0.0.1:%s" % "2558")
+    brightnessSocket.connect("tcp://127.0.0.1:%s" % "2557")
     brightnessSocket.send_string("0")
 
     self.colorThread.join()
     self.modeThread.join()
-    self.pulseThread.join()
     self.brightnessThread.join()
     sys.exit(0)
 
@@ -78,7 +67,7 @@ class SupportThreadController:
     self.colorSocket.bind("tcp://*:%s" % "2555")
     while self.colorThreadToggle:
       command = self.colorSocket.recv_string()
-      print(command)
+      print("Color thread received: " + command)
       args = command.split()
       threading.Thread(target=self.lightControllerShare.changeColor,args=(int(args[0]), int(args[1]), int(args[2]))).start()
       self.colorSocket.send_string("Color OK")
@@ -89,26 +78,18 @@ class SupportThreadController:
     self.modeSocket.bind("tcp://*:%s" % "2556")
     while self.modeThreadToggle:
       command = self.modeSocket.recv_string()
+      print("Mode thread received: " + command)
       args = lightMode.lightChangeDict[command]
       self.lightControllerShare.changeMode(args)
       self.modeSocket.send_string("Mode OK")
     print("Mode thread exiting")
   
-  def pulseThreadOperation(self):
-    print("Pulse thread started")
-    self.pulseSocket.bind("tcp://*:%s" % "2557")
-    while self.pulseThreadToggle:
-      self.pulseSocket.recv_string()
-      self.lightControllerShare.changePulse()
-      threading.Thread(target=self.lightControllerShare.pulseBrightness, args=()).start()
-      self.pulseSocket.send_string("Pulse OK")
-    print("Pulse thread exiting")
-  
   def brightnessThreadOperation(self):
     print("Brightness thread started")
-    self.brightnessSocket.bind("tcp://*:%s" % "2558")
+    self.brightnessSocket.bind("tcp://*:%s" % "2557")
     while self.brightnessThreadToggle:
       command = self.brightnessSocket.recv_string()
+      print("Brightness thread received: " + command)
       threading.Thread(target=self.lightControllerShare.setBrightness, args=(int(command),)).start()
       self.brightnessSocket.send_string("Brightness OK")
     print("Brightness thread exiting")
